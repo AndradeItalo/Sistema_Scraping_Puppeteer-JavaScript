065c905cb3c5b0db7cdbc08362bf77b7657dba22
Italo Andrade <ifa2@cin.ufpe.br>
2024-03-17 16:21:04 -0300

    Refactor: Alterando algumas funcinalidades que estavam duplicadas, e removendo os feedbacks em formatos de listas

--- scraping/api/addProduct.js
+++ scraping/api/addProduct.js
@@ -5,28 +5,26 @@ const { db: prisma } = require('../lib/db');
 async function POST(datas) {
     try {
         for (const data of datas) {
-            const { title, price, description, avaliation, feedbacks, link, image } = data;
-            // Cria um novo registro de produto no banco de dados com os dados coletados.
+            const { title, price, description, avaliation, link, image } = data;
             await prisma.product.create({
                 data: {
                     title: title,
                     price: price,
                     description: description,
                     avaliation: avaliation,
-                    feedbacks: JSON.stringify(feedbacks), // Converte feedbacks em string JSON.
                     link: link,
                     image: image
                 }
             });
         }
 
-        console.log("NOVO PRODUTO"); // Log de sucesso.
+        console.log("NOVO PRODUTO"); 
     } catch (error) {
-        console.error(error); // Caso houver erro durante a inserção.
+        console.error(error); 
     }
 }
 
-// Exporta a função POST para usar no index e fazer a inserção no banco de dados.
+
 module.exports = {
     POST
 };
--- scraping/index.js
+++ scraping/index.js
@@ -2,21 +2,19 @@
 const puppeteer = require('puppeteer');
 const { POST } = require('./api/addProduct.js');
 
-// Define o produto a ser pesquisado e inicializa variáveis para controle de paginação e armazenamento dos produtos
 const product = "teclado";
 let pag = 1;
 const listProduct = [];
 const notAvaliation = 'Não foi avaliado.';
+let search = 2; // CONTROLADOR DE QUANTAS PÁGINAS DESEJA GARIMPAR  -> 1 Pág = 20 produtos
 
 (async () => {
-    // Inicia o navegador com configurações de dimensões para evitar problemas com o layout da página
     const browser = await puppeteer.launch({
         headless: false, 
         defaultViewport: { width: 1200, height: 800 } // PRECISA abrir a tela no minimo nessas dimensões, para não dar erro com layout
     });
     const page = await browser.newPage();
   
-    // Navega até a página inicial da Kabum e realiza a busca pelo produto definido
     await page.goto('https://www.kabum.com.br/');
     await page.waitForSelector('#input-busca');
     await page.type('#input-busca', product); 
@@ -25,7 +23,6 @@ const notAvaliation = 'Não foi avaliado.';
         page.click('.sc-jSgvzq.sc-bkzYnD.bsIXwL.hzpqID')
     ]);
   
-    // Verifica se existe a mensagem de nenhum produto encontrado e encerra a execução se verdadeiro.
     const noProductFound = await page.$('.sc-9e19be64-0.bbNoJo') 
     if (noProductFound) {
       console.log("Nenhum produto encontrado com o INPUT de pesquisa passado.");
@@ -35,12 +32,11 @@ const notAvaliation = 'Não foi avaliado.';
 
     // Loop para navegar pelas páginas de produtos e coletar informações.
     let currentUrl = await page.evaluate(() => window.location.href); 
-    while (pag <= 2) { // AQUI DEFINE QUANTAS PAGINAS DESEJO GARIMPAR
+    while (pag <= search) { // AQUI DEFINE QUANTAS PAGINAS DESEJO GARIMPAR
       console.log('pagina', pag);
       const links = await page.$$eval('.sc-cdc9b13f-7.gHEmMz.productCard > a', el => el.map(link => link.href));
 
       for( const link of links ){
-        // Acessa cada produto individualmente para coletar os detalhes.
         await page.goto(link);
         await page.waitForSelector('.sc-fdfabab6-6.jNQQeD');
   
@@ -65,24 +61,20 @@ const notAvaliation = 'Não foi avaliado.';
           if(!el) return null;
           return el.innerText;
         })
-  
-        const feedbacks = await page.$$eval('.sc-8841b8a3-0.dINqTS', elements => elements.map(el => el.innerText));
 
         // Tira uma screenshot em cada página de produto, e salva na pasta "screenshots" com o titulo do produto.
         const screenshotPath = `screenshots/${title.replace(/[/\\?%*:|"<>]/g, '')}.png`; // Remove caracteres inválidos para o nome do arquivo
         await page.screenshot({path: screenshotPath});
-  
-        // Constrói o objeto do produto e adiciona à lista se título e preço estiverem presentes.
+        
         const obj = {};
         obj.title = title;
         obj.price = price;
         obj.description = description;
         obj.avaliation = avaliation ? avaliation : notAvaliation;
-        obj.feedbacks = feedbacks;
         obj.link = link;
         obj.image = imageUrl;
 
-        // Verifica se o título e o preço não estão vazios.
+        
         if (!obj.title || !obj.price) {
           console.error("Erro: Título ou preço ausente no produto", obj.link);
           continue; 
@@ -94,7 +86,6 @@ const notAvaliation = 'Não foi avaliado.';
       // A paginação só existe na tela principal de listagem de produtos, por isso, pego o URL e sempre que acabar uma página, vou para a página de listagem de produtos.
       await page.goto(currentUrl); 
       
-      // Navega para a próxima página, se houver.
       const hasNextPage = await page.$('.next');
       if (hasNextPage) {
         await Promise.all([
@@ -105,7 +96,7 @@ const notAvaliation = 'Não foi avaliado.';
         currentUrl = await page.url();
         
         } else {
-            break; // Sai do loop se não houver próxima página
+            break; 
         }
         pag++;
     }
@@ -113,6 +104,5 @@ const notAvaliation = 'Não foi avaliado.';
     // Envia os dados coletados para inserção no banco de dados.
     await POST(listProduct);
 
-    // Fecha o navegador.
     await browser.close();
 })();
